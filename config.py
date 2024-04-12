b32f8d1aaf68a92c75c74e57cb3c8718efd77679
pawanjaatpk <118508835+pawanjaatpk@users.noreply.github.com>
2024-04-12 13:58:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ PORT = 6379  # redis port
 PASSWORD = ""  # redis password
 
 PRIVATE_CHAT_ID = -1002069543756  # CHAT WHERE YOU WANT TO STORE VIDEOS
-COOKIE = "browserid=Ybg4sJLP3A7sxyMjJtEnMrAGDvPozzG3q5phm1RmuHqzbwg8TbDAwOVmL-c=; lang=en; __bid_n=18ebd0c6be9cc0d8f14207; __stripe_mid=06e06a24-0f4d-48fb-9a65-ea82b5cc4b2eaac12f; g_state={"i_l":0}; ndus=Yer_bhMteHuiNhl57s1ss2mk1WygF0oUyR-V5qeq; TSID=1UEGhcWvX0ohOdUTi6cxHQgtq5OXJEk4; csrfToken=VxLcpDTgghUQUCpPY-hWsnn5; ndut_fmt=56F81E99245C01B2A12280ECA67530EF93BA2D5C44B7B2F689BED3A1B5CBD6FA"  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
+COOKIE = "csrfToken=bGY31ap5NMpizHanvNzNjjsn; browserid=YdjpbIaXlgC7ZBUFryYviytX28GHwE8WpHoh7sI_mRIxHMKXBWmaAnbAymE=; lang=en; TSID=b1QqnTap2cIvFX2vbb9pFXmFxVMZaraQ; __bid_n=18ed168adc265034a24207; _ga=GA1.1.1748306849.1712910283; g_state={"i_l":0}; ndus=Yer_bhMteHuiY1shSyMOfjpS1_qiolx_zHXgbVBS; ndut_fmt=73661D1EB9C35A8F296528487167759A1230FFB3B541EE7B4EBC89519FB3685C; _ga_06ZNKL8C2E=GS1.1.1712910282.1.1.1712910370.51.0.0"  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
 ADMINS = [5772711610]
